2c9538c0cef0b7d813040451fcfed6e1176da682
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2023-07-12 05:41:53 +0800

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Calculator from './components/Calculator.jsx'
 
-function App() {
+function App () {
   return (
-    <main className="main">
-      <h2>App Component</h2>
-      <Calculator/>
-    </main>
+      <main className="main">
+        <Calculator/>
+      </main>
   )
 }
 
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,7 +4,30 @@ function Calculator () {
   return (
     <section className="calculator section-center">
 
-      <h2>Calculator Component</h2>
+      <div className="calculator-center">
+
+        <div className="calculator-content">
+          <div className="border border-blue-700">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+          </div>
+
+          <div className="border border-black">
+            <div>4</div>
+          </div>
+
+          <div className="border border-black">
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+          </div>
+        </div>
+
+
+      </div>
+
+
     </section>
   )
 }
